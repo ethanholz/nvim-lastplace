374bc9c918acbe21f57fa193a0924183f69f0e86
Ethan Holz <ethan2@vt.edu>
2021-05-03 15:48:59 -0400

    Updated with autocommands and file checking

--- lua/nvim-lastplace/init.lua
+++ lua/nvim-lastplace/init.lua
@@ -10,18 +10,27 @@ vim.fn = vim.fn or setmetatable({}, {
   end,
 })
 
+local function setup()
+	vim.cmd[[autocmd BufWinEnter * lua require'nvim-lastplace'.lastplace_func()]]
+end
+
 local function lastplace_func()
-	if fn.line("'\"") > 0 and fn.line("'\"") <= fn.line("$") then
-		if fn.line("w$") == fn.line("$") then
-			vim.api.nvim_command('normal! g`\"')
-		elseif fn.line("$") - fn.line("'\"") > ((fn.line("w$") - fn.line("w0")) / 2) - 1 then
-			vim.api.nvim_command('normal! g`\"zz')
-		else
-			vim.api.nvim_command([[normal! \G'\"\<c-e>]])
+	-- Check if file exists, if so load
+	local file = fn.empty(fn.glob(fn.expand('%@')))
+	if file then
+		if fn.line("'\"") > 0 and fn.line("'\"") <= fn.line("$") then
+			if fn.line("w$") == fn.line("$") then
+				vim.api.nvim_command('normal! g`\"')
+			elseif fn.line("$") - fn.line("'\"") > ((fn.line("w$") - fn.line("w0")) / 2) - 1 then
+				vim.api.nvim_command('normal! g`\"zz')
+			else
+				vim.api.nvim_command([[normal! \G'\"\<c-e>]])
+			end
 		end
 	end
 end
 
 return {
+	setup = setup,
 	lastplace_func = lastplace_func
 }
