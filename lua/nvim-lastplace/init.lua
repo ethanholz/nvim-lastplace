840b0b0dc0a7d959da4d7c6c84d885c062680232
Hennadii Chernyshchyk <genaloner@gmail.com>
2021-05-23 17:24:37 +0300

    Minor improvements (#2)
    * Use BufReadPost
    
    * Use tbl_contains instead of loop

--- lua/nvim-lastplace/init.lua
+++ lua/nvim-lastplace/init.lua
@@ -3,11 +3,14 @@ local lastplace = {}
 
 function lastplace:setup(options)
 	options = options or {}
-	lastplace.options = options	
+	lastplace.options = options
 	lastplace:set_option("lastplace_ignore_buftype",{'quickfix','nofile','help'})
 	lastplace:set_option("lastplace_ignore_filetype",{'gitcommit','gitrebase','svn','hgcommit'})
 	lastplace:set_option("lastplace_open_folds", 1)
-	vim.cmd[[autocmd BufWinEnter * lua require'nvim-lastplace'.lastplace_func()]]
+	vim.cmd[[augroup NvimLastplace]]
+	vim.cmd[[  autocmd!]]
+	vim.cmd[[  autocmd BufReadPost * lua require'nvim-lastplace'.lastplace_func()]]
+	vim.cmd[[augroup end]]
 end
 
 function lastplace:set_option(option,default)
@@ -20,34 +23,29 @@ function lastplace:set_option(option,default)
 end
 
 function lastplace:lastplace_func()
-	-- Get buffer and filetype
-	local buf = vim.bo.buftype
-	local ft = vim.bo.filetype
-	for i,v in ipairs(lastplace.options.lastplace_ignore_buftype) do
-		if v == buf then return end
+	-- Check if buffer should be ignored
+	if vim.tbl_contains(lastplace.options.lastplace_ignore_buftype,
+			vim.api.nvim_buf_get_option(0, 'buftype')) or
+		vim.tbl_contains(lastplace.options.lastplace_ignore_filetype,
+			vim.api.nvim_buf_get_option(0, 'filetype')) then
+		return
 	end
-	for i,v in ipairs(lastplace.options.lastplace_ignore_filetype) do
-		if v == ft then return end
-	end
-	-- Check if file exists, if so load
-	local file = fn.empty(fn.glob(fn.expand('%@')))
-	if file then
-		-- If the last line is set and the less than the last line in the buffer
-		if fn.line([['"]]) > 0 and fn.line([['"]]) <= fn.line("$") then
-			--Check if the last line of the buffer is the same as the window 
-			if fn.line("w$") == fn.line("$") then
-				--Set line to last line edited
-				vim.api.nvim_command([[normal! g`"]])
+
+	-- If the last line is set and the less than the last line in the buffer
+	if fn.line([['"]]) > 0 and fn.line([['"]]) <= fn.line("$") then
+		--Check if the last line of the buffer is the same as the window 
+		if fn.line("w$") == fn.line("$") then
+			--Set line to last line edited
+			vim.api.nvim_command([[normal! g`"]])
 			-- Try to center
-			elseif fn.line("$") - fn.line([['"]]) > ((fn.line("w$") - fn.line("w0")) / 2) - 1 then
-				vim.api.nvim_command([[normal! g`"zz]])
-			else
-				vim.api.nvim_command([[normal! G'"<c-e>]])
-			end
+		elseif fn.line("$") - fn.line([['"]]) > ((fn.line("w$") - fn.line("w0")) / 2) - 1 then
+			vim.api.nvim_command([[normal! g`"zz]])
+		else
+			vim.api.nvim_command([[normal! G'"<c-e>]])
 		end
-		if fn.foldclosed(".") ~= -1 and lastplace.options.lastplace_open_folds == 1 then
-			vim.api.nvim_command([[normal! zvzz]])
-		end
+	end
+	if fn.foldclosed(".") ~= -1 and lastplace.options.lastplace_open_folds == 1 then
+		vim.api.nvim_command([[normal! zvzz]])
 	end
 end
 
