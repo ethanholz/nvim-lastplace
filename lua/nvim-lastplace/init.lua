30fe710d4417cc67950bbce6b2ec2ac0ff430e12
Andreas Schneider <asn@cryptomilk.org>
2021-10-15 21:57:34 +0200

    Fix lastline with nvim 0.5.1 (#10)

--- lua/nvim-lastplace/init.lua
+++ lua/nvim-lastplace/init.lua
@@ -31,7 +31,9 @@ function lastplace.setup(options)
 	vim.cmd([[augroup NvimLastplace]])
 	vim.cmd([[  autocmd!]])
 	vim.cmd([[  autocmd BufReadPost * lua require('nvim-lastplace').lastplace_buf()]])
-	vim.cmd([[  autocmd FileType * lua require('nvim-lastplace').lastplace_ft()]])
+	if fn.has('nvim-0.5.1') == 0 then
+		vim.cmd([[  autocmd FileType * lua require('nvim-lastplace').lastplace_ft()]])
+	end
 	vim.cmd([[augroup end]])
 end
 
@@ -55,14 +57,23 @@ local set_cursor_position = function()
 end
 
 function lastplace.lastplace_buf()
-	-- If a line has already been specified on the command line, we are done
-	--   nvim file +num
-	if fn.line(".") > 1 then
+	-- Check if the buffer should be ignored
+	if vim.tbl_contains(lastplace.options.lastplace_ignore_buftype, vim.api.nvim_buf_get_option(0, "buftype")) then
 		return
 	end
 
-	-- Check if the buffer should be ignored
-	if vim.tbl_contains(lastplace.options.lastplace_ignore_buftype, vim.api.nvim_buf_get_option(0, "buftype")) then
+	if fn.has('nvim-0.5.1') == 1 then
+		-- Check if the filetype should be ignored
+		if vim.tbl_contains(lastplace.options.lastplace_ignore_filetype, vim.api.nvim_buf_get_option(0, "filetype")) then
+			-- reset cursor to first line
+			vim.api.nvim_command([[normal! gg]])
+			return
+		end
+	end
+
+	-- If a line has already been specified on the command line, we are done
+	--   nvim file +num
+	if fn.line(".") > 1 then
 		return
 	end
 
